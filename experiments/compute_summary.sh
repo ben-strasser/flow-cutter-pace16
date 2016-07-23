d164238b953e37e2eb401cb64373f9f33af52d9c
Ben Strasser <strasser@kit.edu>
2016-07-23 14:16:40 +0200

    Added achieved decompositions

---
+++ experiments/compute_summary.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+(
+	echo "graph,max_bag_size"
+	for G in output/*
+	do
+		F=`basename $G`
+		echo -n ${F%.td}
+		echo -n ,
+		grep -E "^s td" $G | sed -E "s/^s td ([0-9]+) ([0-9]+) ([0-9]+)$/\2/"
+	done
+) > output_summary.csv
